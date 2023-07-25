70cc7c3c090d236c55a58d8b6148c7f71ac4e5dd
Fedor <fedoravdeev3@gmail.com>
2023-07-25 22:45:14 +0700

    f

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from "pinia";
-import App from './App.vue'
-import './styles/main.scss'
-
-const pinia = createPinia();
-
-createApp(App).use(pinia).mount('#app')
